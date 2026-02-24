6c1dda0b99986f2fd1d32816380a99f3fc0ac87e
Jakob Puhl <puhl@kth.se>
2026-02-24 23:04:37 +0100

    refactor some bits

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,14 @@
-use core::panic;
 use bitvec::prelude::*;
-use std::{cmp::Ordering, collections::BinaryHeap, io::{self, Read}};
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
+use std::io::{self, Read};
 
 #[derive(Eq)]
-struct HuffmanNode {
+pub struct HuffmanNode {
     frequency: usize,
     byte: Option<u8>,
     l_child: Option<Box<HuffmanNode>>,
     r_child: Option<Box<HuffmanNode>>,
-}
-
-impl HuffmanNode {
-    // TODO: Maybe take a box directly to avoid overhead of moving the node here
-    fn new(frequency: usize, byte: Option<u8>, l_child: Option<Box<HuffmanNode>>, r_child: Option<Box<HuffmanNode>>) -> Self {
-        HuffmanNode { l_child, r_child, frequency, byte }
-    }
 }
 
 impl PartialEq for HuffmanNode {
@@ -23,10 +17,11 @@ impl PartialEq for HuffmanNode {
     }
 }
 
+// Reverse the order so the the huffman heap becomes a min heap
 impl Ord for HuffmanNode {
     fn cmp(&self, other: &Self) -> Ordering {
         match other.frequency.cmp(&self.frequency) {
-            Ordering::Equal => self.byte.cmp(&other.byte), // Tiebreak by byte value
+            Ordering::Equal => self.byte.cmp(&other.byte),
             ord => ord,
         }
     }
@@ -38,137 +33,151 @@ impl PartialOrd for HuffmanNode {
     }
 }
 
-struct HuffmanEncoder;
-
-impl HuffmanEncoder {
-    pub fn build(data: &[u8]) -> Option<Box<HuffmanNode>> {
-        let frequency = Self::count_frequency(data);
-        
-        let mut heap: BinaryHeap<_> = frequency.iter()
-            .enumerate()
-            .filter(|&(_, &freq)|
-                freq != 0
-            )
-            .map(|(byte, &freq)| 
-                Box::new(
-                    HuffmanNode::new(freq, Some(byte as u8), None, None)
-                )
-            )
-            .collect();
-
-        if heap.len() == 0 {
-            return None;
+impl HuffmanNode {
+    fn new_leaf(frequency: usize, byte: u8) -> Self {
+        Self {
+            frequency,
+            byte: Some(byte),
+            l_child: None,
+            r_child: None,
         }
-        if heap.len() == 1 {
-            panic!("Data with single byte not supported yet")
-        }
-
-        while heap.len() > 1 {
-            let node1 = heap.pop().unwrap();
-            let node2 = heap.pop().unwrap();
-
-            let parent = Box::new(HuffmanNode::new(
-                node1.frequency + node2.frequency,
-                None,
-                Some(node1),
-                Some(node2),
-            ));
-
-            heap.push(parent);
-        }
-
-        let root = heap.pop().unwrap();
-        // Fix incase only one node in heap
-        // let root = match root.byte {
-        //     Some(byte) => root,
-        //     None => Box::new(HuffmanNode::new(
-        //         root.frequency,
-        //         None,
-        //         Some(root),
-        //         None
-        //     )),
-        // };
-
-        Some(root)
     }
 
-    fn count_frequency(data: &[u8]) -> [usize; 256] {
-        let mut frequency = [0 as usize; 256];
-        data.iter().for_each(|&byte| {
-            frequency[byte as usize] += 1;
-        });
-        frequency
+    fn new_internal(left: HuffmanNode, right: HuffmanNode) -> Self {
+        Self {
+            frequency: left.frequency + right.frequency,
+            byte: None,
+            l_child: Some(Box::new(left)),
+            r_child: Some(Box::new(right)),
+        }
     }
 }
 
-struct HuffmanSeralizer {}
+pub struct HuffmanTree {
+    root: Box<HuffmanNode>,
+}
 
-impl HuffmanSeralizer {
-    pub fn serialize(root: &HuffmanNode, data: &[u8]) -> BitVec<u8, Msb0> {
-        let mut content = BitVec::<u8, Msb0>::new();
-        Self::serialize_tree(root, &mut content);
-        let content_len: u64 = content.len() as u64;
-        let data_len: u64 = data.len() as u64;
-
-        let mut header = BitVec::<u8, Msb0>::new();
-        header.extend_from_bitslice(b"JP".view_bits::<Msb0>());
-        header.extend_from_bitslice(content_len.to_be_bytes().view_bits::<Msb0>());
-        header.extend_from_bitslice(data_len.to_be_bytes().view_bits::<Msb0>());
-        header.extend_from_bitslice(&content);
-
-        let mut table: [BitVec<u8, Msb0>; 256] = std::array::from_fn(|_| BitVec::new());
-        Self::build_encoding_table(root, &mut table, &mut BitVec::<u8, Msb0>::new());
-
-        Self::serialize_encoded_data(&table, data, &mut header);
-
-        header
-    }
-
-    fn serialize_tree(root: &HuffmanNode, bitstream: &mut BitVec<u8, Msb0>) {
-        match root.byte {
-            Some(byte) => {
-                bitstream.push(true);
-                bitstream.extend_from_bitslice(byte.view_bits::<Msb0>());
-            },
-            None => { 
-                bitstream.push(false);
-                Self::serialize_tree(root.l_child.as_ref().unwrap(), bitstream);
-                Self::serialize_tree(root.r_child.as_ref().unwrap(), bitstream);
-            }
-        };
-    }
-
-    fn build_encoding_table(root: &HuffmanNode, table: &mut [BitVec<u8, Msb0>; 256], current_encoding: &mut BitVec<u8, Msb0>) {
-        match root.byte {
-            Some(byte) => {
-                table[byte as usize] = current_encoding.clone();
-            },
-            None => {
-                current_encoding.push(false);
-                Self::build_encoding_table(root.l_child.as_ref().unwrap(), table, current_encoding);
-                current_encoding.pop().unwrap();
-
-                current_encoding.push(true);
-                Self::build_encoding_table(root.r_child.as_ref().unwrap(), table, current_encoding);
-                current_encoding.pop().unwrap();
-            }
-        }    
-    }
-
-    fn serialize_encoded_data(table: &[BitVec<u8, Msb0>; 256], data: &[u8], bitstream: &mut BitVec<u8, Msb0>) {
+impl HuffmanTree {
+    pub fn build(data: &[u8]) -> Self {
+        let mut frequency = [0usize; 256];
         for &byte in data {
-            bitstream.extend_from_bitslice(&table[byte as usize]);
+            frequency[byte as usize] += 1;
         }
+
+        let mut heap: BinaryHeap<HuffmanNode> = frequency
+            .iter()
+            .enumerate()
+            .filter(|&(_, &freq)| freq != 0)
+            .map(|(byte, &freq)| HuffmanNode::new_leaf(freq, byte as u8))
+            .collect();
+
+        // If the file is empty, make a dummy node
+        if heap.is_empty() {
+            heap.push(HuffmanNode::new_leaf(0, 0)); 
+        }
+
+        // Edge case where data only has one unique character
+        if heap.len() == 1 {
+            let single_node = heap.pop().unwrap();
+            let dummy_node = HuffmanNode::new_leaf(0, 0);
+            heap.push(HuffmanNode::new_internal(single_node, dummy_node));
+        }
+
+        while heap.len() > 1 {
+            let left = heap.pop().unwrap();
+            let right = heap.pop().unwrap();
+            heap.push(HuffmanNode::new_internal(left, right));
+        }
+
+        Self {
+            root: Box::new(heap.pop().unwrap()),
+        }
+    }
+
+    pub fn serialize(&self) -> BitVec<u8, Msb0> {
+        let mut bitstream = BitVec::<u8, Msb0>::new();
+        Self::serialize_recursive(&self.root, &mut bitstream);
+        bitstream
+    }
+
+    pub fn get_encoding_table(&self) -> Vec<BitVec<u8, Msb0>> {
+        let mut table = vec![BitVec::<u8, Msb0>::new(); 256];
+        Self::build_table_recursive(&self.root, &mut table, &mut BitVec::new());
+        table
+    }
+
+    fn serialize_recursive(node: &HuffmanNode, bitstream: &mut BitVec<u8, Msb0>) {
+        if let Some(byte) = node.byte {
+            bitstream.push(true);
+            bitstream.extend_from_bitslice(byte.view_bits::<Msb0>());
+        } else {
+            bitstream.push(false);
+            if let Some(ref left) = node.l_child {
+                Self::serialize_recursive(left, bitstream);
+            }
+            if let Some(ref right) = node.r_child {
+                Self::serialize_recursive(right, bitstream);
+            }
+        }
+    }
+
+    fn build_table_recursive(
+        node: &HuffmanNode,
+        table: &mut [BitVec<u8, Msb0>],
+        current_encoding: &mut BitVec<u8, Msb0>,
+    ) {
+        if let Some(byte) = node.byte {
+            table[byte as usize] = current_encoding.clone();
+        } else {
+            if let Some(ref left) = node.l_child {
+                current_encoding.push(false);
+                Self::build_table_recursive(left, table, current_encoding);
+                current_encoding.pop();
+            }
+            if let Some(ref right) = node.r_child {
+                current_encoding.push(true);
+                Self::build_table_recursive(right, table, current_encoding);
+                current_encoding.pop();
+            }
+        }
+    }
+}
+
+pub struct HuffmanArchive;
+
+impl HuffmanArchive {
+    pub fn compress(data: &[u8], tree: &HuffmanTree) -> BitVec<u8, Msb0> {
+        let tree_bits = tree.serialize();
+        let encoding_table = tree.get_encoding_table();
+
+        let tree_len = tree_bits.len() as u64;
+        let data_len = data.len() as u64;
+
+        let mut archive = BitVec::<u8, Msb0>::new();
+        
+        // Magic Number
+        archive.extend_from_bitslice(b"JP".view_bits::<Msb0>());
+        
+        // Metadata
+        archive.extend_from_bitslice(tree_len.to_be_bytes().view_bits::<Msb0>());
+        archive.extend_from_bitslice(data_len.to_be_bytes().view_bits::<Msb0>());
+        
+        // Tree Structure
+        archive.extend_from_bitslice(&tree_bits);
+
+        // Encoded Data
+        for &byte in data {
+            archive.extend_from_bitslice(&encoding_table[byte as usize]);
+        }
+
+        archive
     }
 }
 
 fn main() {
-    let mut input = vec![];
-    let error = io::stdin().read_to_end(&mut input);
-    match error {
-        Err(e) => panic!("{}", e),
-        Ok(_) => (),
-    }
+    let mut input = Vec::new();
+    io::stdin().read_to_end(&mut input).expect("Failed to read from stdin");
 
-    
+    let tree = HuffmanTree::build(&input);
+    let compressed_data = HuffmanArchive::compress(&input, &tree);
 }
